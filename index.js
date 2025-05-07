579467db2e03e6b81f170c875d4e9695fcbcae02
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-07 19:37:19 +0530

    express.Router()

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const express = require('express')
+const express=require("express")
 
+const app=express()
 
-const app = express()
+const router=require("./router/router")
 
-app.use(decoration)
+app.use("/",router)
 
-app.get("/", (req, res) => {
-  console.log("user authetication success");
-  res.send("<h1>Success</h1>")
+app.listen(3000,()=>{
+    console.log("the server is runnig 3000")
 })
-
-
-app.listen(3000, () => {
-  console.log("the server is runnig");
-})
-
-function decoration(req,res,next){
-  console.log("user is comming");
-  next()
-}
 
 
 
