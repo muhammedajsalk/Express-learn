33220a1e229fa2a8ef7bd2faed9a6cbb98158810
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-08 11:40:55 +0530

    upload file use express multer

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const express=require("express")
 
 const app=express()
 
-const router=require("./router/router")
+const multer=require('multer')
 
-app.use("/",router)
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
 
-app.listen(3000,()=>{
-    console.log("the server is runnig 3000")
+
+const upload=multer({storage})
+
+app.get("/",(req,res)=>{
+   res.send("home")
 })
 
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.send("succefully post")
+})
 
-
-
-
-
-
+app.listen(3000,()=>{
+    console.log("ther server is runnig 3000")
+})
